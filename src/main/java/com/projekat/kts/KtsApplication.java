9632374ceeadb6728aa326f77dba9510819ddf9c
Predrag Falcic <falcicp@gmail.com>
2017-10-27 11:23:24 +0200

    Empty Project

---
+++ src/main/java/com/projekat/kts/KtsApplication.java
@@ -0,0 +1,12 @@
+package com.projekat.kts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtsApplication.class, args);
+	}
+}
